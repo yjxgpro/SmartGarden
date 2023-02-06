edd4eced0fb7d7ed27c0685f320975b73d0d80a3
yjxgpro <xiaoyc2000@126.com>
2023-02-06 23:56:20 +0000

    soilMoisture

---
+++ soilMoisture.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <wiringPi.h>
+
+#define PIN_0 0
+
+using namespace std;
+
+
+
+int main(int argc, char **argv)
+{
+	wiringPiSetup();
+	
+	pinMode(PIN_0, INPUT);
+	
+	while(1)
+	{
+		if (digitalRead(PIN_0) == HIGH){
+			cout << "dry"<< endl;
+			}
+		else {
+			cout << "wet"<< endl;
+			}
+		delay(2000);
+		}
+	
+	
+	return 0;
+}
