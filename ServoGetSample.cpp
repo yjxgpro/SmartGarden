e4a789309b2307f9809bcdd3290950310b3cb12f
latte-dai <525062601@qq.com>
2023-04-15 16:13:35 +0100

    update servo and detect

--- ServoGetSample.cpp
+++ ServoGetSample.cpp
@@ -1,11 +1,13 @@
 #include "camera.h"
+#include "Servo_new.cpp"
 
 class ServoSampleCallback : public ServoCallback {
     void hasSample(int n1, int n2) {
         Servo servo;
-        servo.init();
+        
         if (n1 > 0 && n2 > 0){
             servo.start(n1, n2);
+            servo.stop();
         }
     }
     
@@ -19,7 +21,14 @@ class ServoSampleCallback : public ServoCallback {
 };
 
 int main(int argc, char* argv[]) {
+    if(gpioInitialise()<0)
+    {
+        return 1;
+    }
     Camera camera;
+    Servo servo;
+    servo.servoInit(servo.gpioPin1);
+    servo.servoInit(servo.gpioPin2);
     ServoSampleCallback servoSampleCallback;
     camera.registerCallback(&servoSampleCallback);
     camera.detect();
---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/ServoGetSample.cpp.o"
+  "CMakeFiles/MyExecutable.dir/Servo_new.cpp.o"
+  "CMakeFiles/MyExecutable.dir/pestDetect.cpp.o"
+  "MyExecutable"
+  "MyExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
