a5a95206dd5d4f6ee66045893f06587dad28f4f3
yjxgpro <xiaoyc2000@126.com>
2023-04-17 20:57:12 +0100

    update main.cpp and CMakeLists

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include "camera.h"
 #include "servo.h"
+#include "BH1750.h"
 
 class ServoSampleCallback : public ServoCallback 
 {
@@ -18,18 +19,41 @@ class ServoSampleCallback : public ServoCallback
     }
 };
 
+class LEDcallback : public BH1750callback
+{
+    public:
+	 void hasSample(float lightvalue)
+	 {
+		float lightneed = 0;
+		if(lightvalue>EnoughLight) 
+		std::cout<<"Enough Light"<<endl;
+		else
+		{
+			std::cout<<"PWM"<<endl;
+			lightneed = EnoughLight-lightvalue;
+			int pwmValue = lightneed * pwmRange / 100;
+             gpioPWM(LEDgpio, pwmValue); // set PWM 
+		}
+	}
+};
 
 int main()
 {
     SoilSensor sensor(GPIO_SOILSENSOR);
+    //BH1750 bh1750test;
+    //LEDcallback LED1;
     sensor.soilthread_start();
     Camera camera;
     ServoSampleCallback servoSampleCallback;
     camera.registerCallback(&servoSampleCallback);
+    //bh1750test.setCallback(&LED1);
+    //bh1750test.setworkmode(Continue_H_M2); 
+    //bh1750test.start();
     camera.detect();
     //while(1)
     //{
     //}
     sensor.soilthread_stop();
+    bh1750test.stop();
     return 0;
 }
