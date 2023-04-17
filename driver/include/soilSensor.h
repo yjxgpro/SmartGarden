f3719714adff185b5c86c48aef8b354f3912a206
latte-dai <63788203+latte-dai@users.noreply.github.com>
2023-04-17 22:26:02 +0100

    Update soilSensor.h

--- driver/include/soilSensor.h
+++ driver/include/soilSensor.h
@@ -1,3 +1,10 @@
+/******************************************************************************
+Author: Shuhan Dai, 2742227d@student.gla.ac.uk
+This file prototypes the SoilSensor class, implemented in soilSensor.cpp.  
+Soil moisture detection and automatic watering is a part function of Project "SmartGarden",
+please click https://github.com/yjxgpro/SmartGarden.git see more.
+******************************************************************************/
+
 #ifndef _SOILSENSOR_H
 #define _SOILSENSOR_H
 
@@ -17,13 +24,14 @@ class SoilSensor
     thread* soil_Thread = NULL;
     
     public:
+    // Passing the detection pin when creating the SoilSensor class
     SoilSensor(int gpioPin);
     ~SoilSensor();
 
     static void run(SoilSensor* soilsensor);
     void soilthread_start();
     void soilthread_stop();
-
+    // register callback funtion
     static void soilcallback(int gpio, int level, uint32_t tick, void *userData);
 
 
